b422385cf76934ac6ba17fa77b7a2db4e35169c6
Alex <46494614+xela-engineer@users.noreply.github.com>
2025-09-06 03:52:19 +0000

    重新排列安裝腳本中的區塊，將 locale 設定和 Ansible 檢查移至安裝過程結束後，並新增 glibc-langpack-en 套件安裝

--- scripts/main.sh
+++ scripts/main.sh
@@ -8,17 +8,6 @@ else
     sed -i 's/OSH_THEME="font"/OSH_THEME="agnoster"/g' ~/.bashrc
 fi
 
-# Fix the locale issue
-if ! grep -q "export LC_ALL=C.UTF-8" ~/.bashrc; then
-    echo "Setting locale to C.UTF-8"
-    echo "export LC_ALL=C.UTF-8" >> ~/.bashrc
-fi
-ansible --help > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-    echo "Ansible installation failed. Please check the installation logs."
-    exit 1
-fi
-
 echo "Installing Repo"
 
 if [ ! -f ../inventory/repos/rhel8/rhel.repo ]; then
@@ -29,7 +18,18 @@ touch /etc/yum.repos.d/rhel.repo
 cp ../inventory/repos/rhel8/rhel.repo /etc/yum.repos.d/rhel.repo
 
 # Install jq
-dnf install -y xorg-x11-xauth jq
+dnf install -y xorg-x11-xauth jq glibc-langpack-en
 
 
 echo "Installation completed"
+
+# Fix the locale issue
+if ! grep -q "export LC_ALL=C.UTF-8" ~/.bashrc; then
+    echo "Setting locale to C.UTF-8"
+    echo "export LC_ALL=C.UTF-8" >> ~/.bashrc
+fi
+ansible --help > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Ansible installation failed. Please check the installation logs."
+    exit 1
+fi
