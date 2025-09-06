b205561795019a9b4b3e4a4bcbe02dadc13b4695
Alex <46494614+xela-engineer@users.noreply.github.com>
2025-09-06 03:33:54 +0000

    Refactor installation script to check for existing repository before cloning

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
-git clone https://github.com/xela-engineer/public-rhel-install.git
+if [ ! -d "public-rhel-install" ]; then
+  git clone https://github.com/xela-engineer/public-rhel-install.git
+else
+    git -C public-rhel-install pull --force
+fi
 ./public-rhel-install/scripts/main.sh
